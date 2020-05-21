97f5d3056c95d6121e4ef8f6ebc800579e624968
IrinaDyakonova <dyakonova.irina.v@gmail.com>
2020-05-21 10:08:45 +0300

    hw_26_with_change_by_comments

--- src/main/java/mate/academy/cinemaproject/dao/impl/MovieDaoImpl.java
+++ src/main/java/mate/academy/cinemaproject/dao/impl/MovieDaoImpl.java
@@ -20,9 +20,10 @@ public class MovieDaoImpl implements MovieDao {
         Transaction transaction = null;
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             transaction = session.beginTransaction();
-            Long itemId = (Long) session.save(movie);
+            Long movieId = (Long) session.save(movie);
             transaction.commit();
-            movie.setId(itemId);
+            movie.setId(movieId);
+            LOGGER.info("Movie insert");
             return movie;
         } catch (Exception e) {
             if (transaction != null) {
